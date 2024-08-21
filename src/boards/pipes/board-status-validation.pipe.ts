31a19e182ee1844b07e0714342da8b4525f41ac7
Geun Won Ma <wonny@Geunui-MacBookAir.local>
2024-08-21 13:50:51 +0900

    Feat: add status pipe validation

---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,26 @@
+import { BadRequestException, PipeTransform } from "@nestjs/common";
+import { BoardStatus } from "../board.model";
+
+// 커스텀 파이프를 사용해서 status에 public, private 만 들어가고 나머지 에러 처리하는 유효성 검사
+export class BoardStatusValidationPipe implements PipeTransform {
+    readonly StatusOptions = [
+        BoardStatus.PRIVATE,
+        BoardStatus.PUBLIC
+    ]
+
+    transform(value: any) {
+        value = value.toUpperCase();
+
+        if(!this.isStatusValid(value)) {
+            throw new BadRequestException(`${value} isn't in the status options`)
+        }
+
+        return value;
+    }
+
+    private isStatusValid(status: any) {
+        const index = this.StatusOptions.indexOf(status);
+        return index !== -1
+    }
+}
+
