7f3ff4079dc775ab6a7748dd3e5e55c4816249de
Geun Won Ma <wonny@Geunui-MacBookAir.local>
2024-08-27 17:37:22 +0900

    Feat: 커스텀 데코레이터로 user 정보만 나오게 설정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './get-user.decorator';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -19,9 +21,10 @@ export class AuthController {
         return this.authService.signIn(authCredentialsDto);
     }
 
+    // 커스텀 데코레이터 사용(user 객체만 나오게)
     @Post('/test')
     @UseGuards(AuthGuard())
-    test(@Req() req) {
-        console.log('req', req);
+    test(@GetUser() user: User) {
+        console.log('user', user);
     }
 }
